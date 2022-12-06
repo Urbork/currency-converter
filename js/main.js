0e3f7bd1d37f4d576e33bca61fe5791a03db17d0
Jonasz Ja <jajonaszjj@gmail.com>
2022-12-06 10:09:27 +0100

    functions added

--- js/main.js
+++ js/main.js
@@ -1,38 +1,97 @@
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-result");
+// {
+//   const amountElement = document.querySelector(".js-amount");
+//   const currencyElement = document.querySelector(".js-currency");
+//   const formElement = document.querySelector(".js-form");
+//   const resultElement = document.querySelector(".js-result");
 
-let eurRate = 4.66;
-let usdRate = 4.5;
-let gbpRate = 5.4;
-let chfRate = 4.74;
+//   const eurRate = 4.66;
+//   const usdRate = 4.5;
+//   const gbpRate = 5.4;
+//   const chfRate = 4.74;
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
+//   formElement.addEventListener("submit", (event) => {
+//     event.preventDefault();
 
-  let amount = amountElement.value;
-  let currency = currencyElement.value;
-  let currencyRate;
+//     const amount = amountElement.value;
+//     const currency = currencyElement.value;
+//     let currencyRate;
 
-  switch (currency) {
-    case "EUR":
-      currencyRate = eurRate;
-      break;
-    case "USD":
-      currencyRate = usdRate;
-      break;
-    case "GBP":
-      currencyRate = gbpRate;
-      break;
-    case "CHF":
-      currencyRate = chfRate;
-  }
+//     switch (currency) {
+//       case "EUR":
+//         currencyRate = eurRate;
+//         break;
+//       case "USD":
+//         currencyRate = usdRate;
+//         break;
+//       case "GBP":
+//         currencyRate = gbpRate;
+//         break;
+//       case "CHF":
+//         currencyRate = chfRate;
+//     }
 
-  let result = amount / currencyRate;
-  resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency}`;
-});
+//     const result = amount / currencyRate;
+//     resultElement.innerText = `${amount} PLN = ${result.toFixed(
+//       2
+//     )} ${currency}`;
+//   });
 
-formElement.addEventListener("reset", () => {
-  resultElement.innerText = "N/A";
-});
+//   formElement.addEventListener("reset", () => {
+//     resultElement.innerText = "N/A";
+//   });
+// }
+{
+  const calculateResult = (amount, currency) => {
+    const eurRate = 4.66;
+    const usdRate = 4.5;
+    const gbpRate = 5.4;
+    const chfRate = 4.74;
+
+    switch (currency) {
+      case "EUR":
+        return amount / eurRate;
+      case "USD":
+        return amount / usdRate;
+      case "GBP":
+        return amount / gbpRate;
+      case "CHF":
+        return amount / chfRate;
+    }
+  };
+
+  const resultElement = document.querySelector(".js-result");
+
+  const updateResultText = (amount, result, currency) => {
+    const resultAccuracy = 2;
+
+    resultElement.innerText = `${amount} PLN = ${result.toFixed(
+      resultAccuracy
+    )} ${currency}`;
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
+
+    const result = calculateResult(amount, currency);
+
+    updateResultText(amount, result, currency);
+  };
+
+  const onFormReset = () => {
+    resultElement.innerText = "N/A";
+  };
+
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit);
+    formElement.addEventListener("reset", onFormReset);
+  };
+
+  init();
+}
